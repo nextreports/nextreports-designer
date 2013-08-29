1a1ac2237862606c23ada3418bc0f1ff3080fd6f
Decebal Suiu <decebal.suiu@gmail.com>
2013-08-29 16:00:40 +0300

    extract setup tasks in setup.xml

--- src/ro/nextreports/designer/ui/sqleditor/syntax/SqlLexer.java
+++ src/ro/nextreports/designer/ui/sqleditor/syntax/SqlLexer.java
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package ro.nextreports.designer.ui.sqleditor.syntax;
+
 public final class SqlLexer extends AbstractLexer {
 
   /** This character denotes the end of file */
